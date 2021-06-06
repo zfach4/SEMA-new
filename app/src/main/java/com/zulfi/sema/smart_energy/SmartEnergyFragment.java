09e30b0e0e430f5eb5f4f4e5ac6fbdad859b9031
Intan Nurjanah <intan@gits.id>
2021-06-06 18:43:25 +0700

    - tambah 2 button di home - ubah layout container untuk fragment biar ga ketutup action bar - tambah logic di smart energy fragment - tambah layout dan logic di smart light fragment - disable back button di login activity

--- app/src/main/java/com/zulfi/sema/smart_energy/SmartEnergyFragment.java
+++ app/src/main/java/com/zulfi/sema/smart_energy/SmartEnergyFragment.java
@@ -11,12 +11,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.zulfi.sema.R;
 
 public class SmartEnergyFragment extends Fragment {
 
     private SmartEnergyViewModel mViewModel;
+
+    private Switch switchMode;
+    private TextView tvMode;
+    private View manualContentView;
+    private View autoContentView;
 
     public static SmartEnergyFragment newInstance() {
         return new SmartEnergyFragment();
@@ -25,7 +33,20 @@ public class SmartEnergyFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_smart_energy, container, false);
+        View view = inflater.inflate(R.layout.fragment_smart_energy, container, false);
+
+        manualContentView = view.findViewById(R.id.content_manual);
+        autoContentView = view.findViewById(R.id.content_auto);
+
+        tvMode = view.findViewById(R.id.tv_mode_state);
+        switchMode = view.findViewById(R.id.switch_mode);
+        switchMode.setOnCheckedChangeListener((buttonView, isChecked) -> setContentMode(isChecked));
+
+        // set initial state
+        // harusnya ngambil data dari server
+        setContentMode(switchMode.isChecked());
+
+        return view;
     }
 
     @Override
@@ -33,6 +54,18 @@ public class SmartEnergyFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(SmartEnergyViewModel.class);
         // TODO: Use the ViewModel
+    }
+
+    private void setContentMode(Boolean isChecked) {
+        if (isChecked) { // otomatis
+            tvMode.setText(R.string.auto_title);
+            manualContentView.setVisibility(View.GONE);
+            autoContentView.setVisibility(View.VISIBLE);
+        } else { // manual
+            tvMode.setText(R.string.manual_title);
+            manualContentView.setVisibility(View.VISIBLE);
+            autoContentView.setVisibility(View.GONE);
+        }
     }
 
 }
