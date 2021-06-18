7990155721a24ca70a371cdc67feb08f5e71c786
zfach4 <zfach12@gmail.com>
2021-06-18 07:11:17 +0700

    parsing data login

--- app/src/main/java/com/zulfi/sema/LoginActivity.java
+++ app/src/main/java/com/zulfi/sema/LoginActivity.java
@@ -2,9 +2,17 @@ package com.zulfi.sema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btn_login;
+    private EditText et_username, et_password;
+    private String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +20,35 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         setTitle(R.string.title_activity_login);
+        initView();
+        setData();
     }
 
     @Override
     public void onBackPressed() {
         // untuk disable button back
         return;
+    }
+
+    public void initView(){
+        btn_login = findViewById(R.id.btn_login);
+        et_username = findViewById(R.id.et_username);
+        et_password = findViewById(R.id.et_password);
+    }
+
+    public void setData(){
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                username = et_username.getText().toString();
+                password = et_password.getText().toString();
+                // action
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.putExtra("username", username);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }
+        });
     }
 }
